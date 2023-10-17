a12c61f84bf4fa9607d556ad9e5ae934c9a1e60f
catarina sayuri <catarina.arashiro@gmail.com>
2023-10-17 17:19:18 -0300

    js

--- Sprint3-Framework/imc14/js/script.js
+++ Sprint3-Framework/imc14/js/script.js
@@ -1,4 +1,72 @@
-let listaPessoas; //undefined
+const listaPessoas = []; //undefined
+
+function calcular(e) 
+{
+    e.preventDefault();
+
+    let nome = document.getElementById("nome").value.trim() ;
+    let altura = parseFloat(document.getElementById("altura").value );
+    let peso = parseFloat(document.getElementById("peso").value );
+
+    if(isNaN(altura) || isNaN(peso) || nome.length < 2)
+    {alert("É necessário preencher todos os campos corretamente") 
+    return;
+    }
+
+    const imc = calcularImc(peso, altura);    
+    const txtSituacao = geraSituacao(imc);
+
+    const pessoa = {
+        nome, //: nome, quando o nome da variavel e o mesmo do objeto voce pode retira-lo
+        altura, //: altura,
+        peso, //: peso,
+        imc, //: imc,
+        situacao : txtSituacao
+    }
+
+            //cadastra na lista de pessoas
+            listaPessoas.push(pessoa);
+            localStorage.setItem("listaPessoas", JSON.stringify(listaPessoas));
+            //exibir a pessoa na tela
+            exibirPessoas();
+
+}
+
+function calcularImc(peso , altura) 
+{
+    return peso / Math.pow(altura, 2);
+    // return peso / (altura * altura);    
+    // return peso / (altura ** 2);    
+}
+
+function geraSituacao(imc) 
+{
+    if (imc <= 18.5) 
+    {
+        return "Abaixo do Peso"
+    }
+    else if (imc <= 24.9) 
+    {
+        return "Peso Ideal"
+    }
+    else if (imc <= 29.9) 
+    {
+        return "Acima do Peso"
+    }
+    else if (imc <= 34.9) 
+    {
+        return "Obesidade I"
+    }
+    else if (imc <= 39.9 ) 
+    {
+        return "Obesidade II"
+    }
+    else if (imc >= 40 ) 
+    {
+        return "Obesidade III"
+    }
+    
+}   
 
 if (localStorage.getItem("listaPessoas") == null) { //não tem dados
     listaPessoas = []; //inicializa com array vazio
@@ -7,77 +75,28 @@ if (localStorage.getItem("listaPessoas") == null) { //não tem dados
     listaPessoas = JSON.parse(localStorage.getItem("listaPessoas"));
 }
 
-exibeResultado(); //roda a função pra exibir os cadastros, caso existam
+exibirPessoas(); //roda a função pra exibir os cadastros, caso existam
+    /* <tr>
+    <td>Matheus</td>
+    <td>1.68</td>
+    <td>60</td>
+    <td>21</td>
+    <td>Peso Ideal</td>
+    </tr> */
+function exibirPessoas() {
+    let linhas = "";
 
-
-function calculaIMC(a, p) {
-    return p / (a * a);
-}
-
-function geraSituacao(imc) {
-
-    if (imc < 18.5) {
-        return 'Magreza Severa';
-    } else if (imc >= 18.5 && imc <= 24.99) {
-        return 'Peso normal';
-    } else if (imc >= 25 && imc <= 29.99) {
-        return "Acima do peso";
-    } else if (imc >= 30 && imc <= 34.99) {
-        return 'Obesidade I';
-    } else if (imc >= 35 && imc <= 39.99) {
-        return 'Obesidade II (severa)';
-    } else { //a partir de 40
-        return 'Cuidado!!!';
-    }
-}
-
-function calcular() {
-
-    //pegar os dados do formulário
-    let nome = document.getElementById('nome').value;
-    let altura = parseFloat(document.getElementById('altura').value);
-    let peso = parseFloat(document.getElementById('peso').value);
-
-    if (nome == "" || isNaN(altura) || isNaN(peso)) { //esqueceu campo sem preencher
-        alert("Preencha todos os campos!");
-    } else { //tudo preenchido
-
-        //calcualar o imc
-        let imc = calculaIMC(altura, peso);
-        //gerar a situação, baseada no imc
-        let situacao = geraSituacao(imc);
-
-
-        //guardar o imc e a situação no objeto pessoa
-        let pessoa = {};
-        pessoa.nome = nome;
-        pessoa.altura = altura;
-        pessoa.peso = peso;
-        pessoa.imc = imc;
-        pessoa.situacao = situacao;
-
-        //cadastra na lista de pessoas
-        listaPessoas.push(pessoa);
-        localStorage.setItem("listaPessoas", JSON.stringify(listaPessoas));
-        //exibir a pessoa na tela
-        exibeResultado();
-    }
-
-} //fim da função calcular
-
-function exibeResultado() {
-    let template = "";
-
-    for (let i = 0; i < listaPessoas.length; i++) {
-        template += `<tr>
-                        <td>${listaPessoas[i].nome}</td>
-                        <td>${listaPessoas[i].altura}</td>
-                        <td>${listaPessoas[i].peso}</td>
-                        <td>${listaPessoas[i].imc}</td>
-                        <td>${listaPessoas[i].situacao}</td>
-                    </tr>`;
-
-    }
-
-    document.getElementById('cadastro').innerHTML = template;
+    listaPessoas.forEach(function (oPessoa) {
+        //linhas de tabela
+        linhas += `
+        <tr>
+        <td>${oPessoa.nome}</td>
+        <td>${oPessoa.altura}</td>
+        <td>${oPessoa.peso}</td>
+        <td>${oPessoa.imc.toFixed(2)}</td>
+        <td>${oPessoa.situacao}</td>
+        </tr>
+        `;
+        document.getElementById('cadastro').innerHTML = linhas;
+    })
 }
