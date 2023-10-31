94f743228c51e9e43202e46cfd55db7c221443c4
Catarina Sayuri Arashiro Braga Felipe <40666090823@info.local>
2023-10-31 17:06:49 -0300

    js

--- Sprint3-Framework/react/hello/src/App.js
+++ Sprint3-Framework/react/hello/src/App.js
@@ -1,24 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Contador from './components/Contador/Contador';
+import Rotas from './route';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Rotas />
+    //criar o projeto event+ (create-react-app)
+    //instalar o react-router-dom
+    //rodar o projeto e ver se estar ok    
   );
 }
 
---
+++ Sprint3-Framework/react/hello/src/Pages/HomePage/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './HomePage.css';
+
+const HomePage = () => {
+    return(
+        <div>
+            
+        </div>
+    );
+}
+
+export default HomePage;
+
